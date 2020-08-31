4a92b708994b38afacdc0c514f33d65855fc9555
Halley Paulson <paul1098@morris.umn.edu>
2020-08-31 16:54:21 -0500

    Final script

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -2,9 +2,8 @@
 #Script for wraping text in a header and footer
 #Takes 3 arguments (file.txt specifier destinationFile.html)
 
-textToWrap="$1"
-header=$(touch "$2"_header.html)
-footer=$(touch "$2"_footer.html)
-destinationFile=$(touch "$3")
+header="$2"_header.html
+footer="$2"_footer.html
+touch "$3"
 
-cat "$header" "$textToWrap" "$footer" > "$destinationFile"
+cat "$header" "$1" "$footer" > "$3"
